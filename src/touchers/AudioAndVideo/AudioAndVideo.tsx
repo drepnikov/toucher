57979c4cb43ef6faab5454cefc36d7595254d456
dvrepa <d.v.repnikov@gmail.com>
2022-03-29 09:58:48 +0300

    Потрогал захват камеры

--- src/touchers/AudioAndVideo/AudioAndVideo.tsx
+++ src/touchers/AudioAndVideo/AudioAndVideo.tsx
@@ -1,27 +1,54 @@
 import * as React from "react";
-import { Box } from "@mui/material";
-import { useEffect } from "react";
+import { Box, Button } from "@mui/material";
+import { useEffect, useRef, useState } from "react";
 import { Header } from "src/core/components/Header/Header";
 
 interface IVideochatProps {}
 
 const AudioAndVideo: React.FC<IVideochatProps> = () => {
-    useEffect(() => {
-        console.log(navigator);
-    }, []);
+    const cameraRef = useRef<HTMLVideoElement>(null);
+    const [cameraNotFound, setCameraNotFound] = useState(false);
+
+    const getVideo = () => {
+        navigator.mediaDevices
+            .getUserMedia({
+                video: {
+                    width: 300,
+                },
+            })
+            .then((stream) => {
+                if (cameraRef.current) {
+                    cameraRef.current.srcObject = stream;
+                    cameraRef.current.play();
+                }
+            })
+            .catch((e) => {
+                setCameraNotFound(true);
+            });
+    };
 
     return (
         <Box sx={{ pt: "10px" }}>
             <Header>Просто тег видео:</Header>
 
-            <p>
+            <Box>
                 <video
                     src={"https://2ch.hk/b/src/265636675/16484714773930.webm"}
                     controls={true}
                 />
-            </p>
+            </Box>
 
             <Header>Захват с камеры:</Header>
+
+            <Box>
+                <Button onClick={() => getVideo()}>Включить камеру</Button>
+
+                {cameraNotFound && <p>Камера не обнаружена</p>}
+
+                <Box>
+                    <video ref={cameraRef} controls={true} />
+                </Box>
+            </Box>
         </Box>
     );
 };
